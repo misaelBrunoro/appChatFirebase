1a4e9ca7bd182f1e1c5a68f2d6edfa340f39db89
misaewl3 <misaelbrunoroliberato@gmail.com>
2019-11-03 18:19:11 -0300

    updates

--- app/src/main/java/com/misael/appchat/RegisterActivity.java
+++ app/src/main/java/com/misael/appchat/RegisterActivity.java
@@ -1,27 +1,46 @@
 package com.misael.appchat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.misael.appchat.R;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText mEditUsername;
     private EditText mEditEmail;
     private EditText mEditPassword;
     private Button mBtnRegister;
+    private Button mBtnPhoto;
+    private ImageView mImgPhoto;
+
+    private Uri mSelectedURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +51,15 @@ public class RegisterActivity extends AppCompatActivity {
         mEditEmail       = findViewById(R.id.editEmail);
         mEditPassword    = findViewById(R.id.editPassword);
         mBtnRegister     = findViewById(R.id.btnRegister);
+        mBtnPhoto        = findViewById(R.id.btnPhoto);
+        mImgPhoto        = findViewById(R.id.imgPhoto);
+
+        mBtnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectPhoto();
+            }
+        });
 
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,12 +69,44 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            mSelectedURI = data.getData();
+
+            Bitmap bitmap = null;
+            try {
+                if (android.os.Build.VERSION.SDK_INT >= 29) {
+                    // To handle deprication use
+                    bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(getContentResolver(), mSelectedURI));
+                } else{
+                    // Use older version
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), mSelectedURI);
+                }
+                mImgPhoto.setImageDrawable(new BitmapDrawable(this.getResources(), bitmap));
+                mBtnPhoto.setAlpha(0);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void selectPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+
+    }
+
     private void registerUser() {
+        String username = mEditUsername.getText().toString();
         String email    = mEditEmail.getText().toString();
         String password = mEditPassword.getText().toString();
 
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Você deve preencher a senha e o email!", Toast.LENGTH_SHORT).show();
+        if (email.isEmpty() || password.isEmpty() || username.isEmpty()) {
+            Toast.makeText(this, "Você deve preencher todos os campos", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -56,6 +116,7 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Log.i("Teste", task.getResult().getUser().getUid());
+                            savePhotoInFirebase();
                         }
                     }
                 })
@@ -63,6 +124,29 @@ public class RegisterActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.i("Teste", e.getMessage());
+                    }
+                });
+    }
+
+    private void savePhotoInFirebase() {
+        String fileName = UUID.randomUUID().toString();
+        final StorageReference ref = FirebaseStorage.getInstance().getReference("/Images/" + fileName);
+        ref.putFile(mSelectedURI)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Log.i("Teste", uri.toString());
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Teste", e.getMessage(), e);
                     }
                 });
     }
